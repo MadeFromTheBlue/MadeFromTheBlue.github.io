e7055b7cf48bebf37b26a97a121743b9ec632569
Sam Sartor <eeky.sam@gmail.com>
2015-07-04 13:00:39 -0600

    Animated Logo
    Remove the canvas
    Animate logo

--- js/graphics.js
+++ js/graphics.js
@@ -1,25 +1,15 @@
-var sqrt2 = Math.sqrt(2);
-var angle = Math.atan2(1, sqrt2);
-
-function draw(canvas) {
-    var mw = Math.min(canvas.width, canvas.height);
-    var ctx = canvas.getContext('2d');
-    ctx.transform(mw / 3, 0, 0, mw / 3, canvas.width / 2, mw / 2);
-    ctx.rotate(angle);
-    ctx.rect(-1 / 2, -sqrt2 / 2, 1, sqrt2);
-    ctx.fillStyle = "#3250FF";
-    ctx.fill();
-}
-
-(function (draw) {
-    var canvas = document.getElementById('canvas');
+(function () {
+    var logo = document.getElementById('logo');
 
     window.addEventListener('resize', resize, false);
 
-    function resize() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        draw(canvas);
+    function resize() {        
+        var mw = Math.min(window.innerWidth, window.innerHeight);
+        
+        logo.width = mw / 2;
+        logo.height = mw / 2;
+        logo.style.left = ((window.innerWidth - logo.width) / 2) + "px";
+        logo.style.top = logo.height / 4 + "px";
     }
     resize();
-})(draw);
+})();
