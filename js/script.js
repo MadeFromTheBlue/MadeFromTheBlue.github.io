bdcbf9857207dc45b5f79d47d14bbbb14f53e34a
Sam Sartor <eeky.sam@gmail.com>
2015-07-07 13:00:49 -0600

    Scroll Control
    Control banner with scroll

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
-var content = document.getElementById('content');
-var tbar = document.getElementById('tbar');
-
 window.addEventListener('resize', resize, false);
+window.addEventListener('scroll', scroll);
 
-var angles = [0, 0.194, 0.782, 1.764, 3.134, 4.875, 6.958, 9.339, 11.96, 14.75, 17.627, 20.504, 23.294, 25.915, 28.296, 30.379, 32.12, 33.49, 34.472, 35.06, 35.254];
+//var angles = [0, 0.194, 0.782, 1.764, 3.134, 4.875, 6.958, 9.339, 11.96, 14.75, 17.627, 20.504, 23.294, 25.915, 28.296, 30.379, 32.12, 33.49, 34.472, 35.06, 35.254];
+var tbarhs = [0.25, 0.261728, 0.297255, 0.356516, 0.439013, 0.543483, 0.667806, 0.808825, 0.962435, 1.12373, 1.28726, 1.44754, 1.5995, 1.7388, 1.8622, 1.96753, 2.05358, 2.11996, 2.16679, 2.19454, 2.20364];
 
 var centers = [];
 var edges = [];
 
-var mousex;
-var mousey;
-
 var smallDim;
 
-function readMouse(e) {
-    mousex = e.clientX;
-    mousey = e.clientY;
-}
-document.onmousemove = readMouse;
+var tbarheight = 250;
+var tbarcimheight = 312;
+var tbarcimfheight = 352.6;
 
 (function () {
-    for (var i = 0; i < 21; i++) {
+    for (var i = 0; i < tbarhs.length; i++) {
         var time = i * 4;
         var timec = "00";
         if (time < 10) {
@@ -37,62 +31,73 @@ document.onmousemove = readMouse;
     }
 })();
 
-function calch(angle) {
-    angle = (angle) / 180 * Math.PI;
-    return Math.cos(angle) * 0.25 + Math.sin(angle) * 2 * Math.sqrt(3);
-}
+function drawtbar() {
+    var tbar = $('#tbar')[0];
 
-function drawtbar(time, scale) {
-    if (time < 0) {
-        time = 0;
-    } else if (time > 1) {
-        time = 1;
+    var pos = document.body.scrollTop;
+    var time = 0;
+
+    function objecttopx(x) {
+        return x * 160 * (tbarheight / tbarcimfheight);
     }
 
-    var itime = Math.round((time * 80) / 4);
+    tbar.height = Math.max(tbarheight - pos, objecttopx(0.25));
+    tbar.style.top = pos + "px";
 
-    var center = centers[itime];
-    var edge = edges[itime];
+    if (tbar.height <= objecttopx(tbarhs[0])) {
+        time = 0;
+    } else if (tbar.height >= objecttopx(tbarhs[tbarhs.length - 1])) {
+        time = tbarhs.length - 1;
+    } else {
+        while (true) {
+            if (tbar.height < (objecttopx(tbarhs[time]) + objecttopx(tbarhs[time + 1])) / 2) {
+                break;
+            } else {
+                time++;
+            }
+        }
+    }
 
-    var imh = scale;
+    var center = centers[time];
+    var edge = edges[time];
+
+    var imh = tbarheight;
     var imw = imh * center.width / center.height;
 
-    var h = calch(angles[itime]);
-    tbar.height = h * 160 * (scale / center.height);
-
-    var simh = scale * edge.height / center.height;
+    var simh = tbarheight * edge.height / center.height;
     var simw = (tbar.width - imw) / 2;
 
     var ctx = tbar.getContext("2d");
     ctx.drawImage(center, simw, (tbar.height - imh) / 2, imw, imh);
-    simw *= Math.cos(time * Math.PI / 2);
+    simw *= Math.cos(time / (tbarhs.length - 1) * Math.PI / 2);
     var y = (tbar.height - simh) / 2;
-    ctx.drawImage(edge, 0, y, simw, simh);
-    ctx.drawImage(edge, tbar.width - simw, y, simw, simh);
+    ctx.drawImage(edge, 1, 0, edge.width - 2, edge.height, 0, y, simw, simh);
+    ctx.drawImage(edge, 1, 0, edge.width - 2, edge.height, tbar.width - simw, y, simw, simh);
 }
 
-function centerLeft(width) {
-    return ((window.innerWidth - width) / 2) + "px";
+function scroll() {
+    drawtbar();
 }
 
 function resize() {
-    smallDim = Math.min(window.innerWidth, window.innerHeight);
+    var tbar = $('#tbar');
+    var width = $('body').innerWidth();
 
-    tbar.width = window.innerWidth;
-    content.style.width = window.innerWidth + "px";
+    tbar[0].width = width;
+
+    var contentbox = $('#content_box');
+    contentbox.css('width', width + 'px');
+    contentbox.css('top', tbarheight + 'px');
+    contentbox.css('height', window.innerHeight * 2 + 'px');
+
+    var content = $('#content');
+    var marg = 20;
+    content.css('margin', width - 2 * marg + 'px');
+    content.css('margin', marg + 'px');
+
+    scroll();
 }
 
 resize();
 
-setInterval(function () {
-    if (mousex)
-    {
-        drawtbar(1 - (4 * (mousey - 50) / window.innerHeight), Math.min(smallDim, 250));
-    }
-    else
-    {
-        drawtbar(0, Math.min(smallDim, 250));
-    }
-    content.style.top = tbar.height + "px";
-    content.style.height = window.innerHeight - tbar.height -5 + "px";
-}, 100);
+centers[centers.length - 1].addEventListener("load", drawtbar, false);
